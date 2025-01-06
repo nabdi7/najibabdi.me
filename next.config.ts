f6266fdc4c1293937d5b325da55e076c7e6f4fe6
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2025-01-05 16:51:52 -0800

    project setup

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  // output: 'export',
+  output: 'export',
   images: {unoptimized: true},
 
   // images: {
