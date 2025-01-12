0ea8605b3ddd526fd333676dedb7ab718d7b1935
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2025-01-11 21:57:41 -0800

    fixed errors

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,34 +1,15 @@
-import type { Metadata } from "next/types";
+import Link from "next/link";
+import Image from "next/image";
+import { formatDate } from "@/lib/utils";
+import MDXContent from "@/components/blogs/mdx-content";
 import { getPosts, getPostBySlug } from "@/lib/posts";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import { notFound } from "next/navigation";
-import PostPage from "./PostPage";
+import Subscribe from "@/components/subscribe/Subscribe";
 
-type Props = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | undefined }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const post = await getPostBySlug(params.slug);
-  
-  if (!post) {
-    return {
-      title: 'Blog Not Found'
-    };
-  }
-
-  return {
-    title: post.metadata.title || 'Blog Post',
-    description: post.metadata.summary || '',
-    openGraph: {
-      title: post.metadata.title || 'Blog Post',
-      description: post.metadata.summary || '',
-      type: 'article',
-      authors: [post.metadata.author || ''],
-      publishedTime: post.metadata.publishedAt,
-    },
-  };
-}
+type PageProps = {
+  params: { slug: string };
+};
 
 export async function generateStaticParams() {
   const posts = await getPosts();
@@ -37,7 +18,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function Page({ params }: Props) {
+export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const post = await getPostBySlug(slug);
 
@@ -45,8 +26,60 @@ export default async function Page({ params }: Props) {
     notFound();
   }
 
-  return <PostPage post={post} />;
+  const { metadata, content } = post;
+  const { title, image, author, publishedAt } = metadata;
+
+  return (
+    <section className="pb-24 pt-32">
+      <div className="container max-w-3xl">
+        <Link
+          href="/blogs"
+          className="mb-8 inline-flex items-center gap-2 text-sm font-light text-muted-foreground transition-colors hover:text-foreground"
+        >
+          <ArrowLeftIcon className="h-5 w-5" />
+          <span>Back to blogs</span>
+        </Link>
+
+        {image && (
+          <div className="relative mb-6 h-96 w-full overflow-hidden rounded-lg">
+            <Image
+              src={image}
+              alt={title || ""}
+              className="object-cover"
+              fill
+            />
+          </div>
+        )}
+
+        <header>
+          <h1 className="title no-underline">{title}</h1>
+          <div className="flex items-center space-x-4 mt-3">
+            <div className="relative h-10 w-10 overflow-hidden rounded-full">
+              <Image
+                src="/najib.jpg"
+                alt={author || "Author"}
+                className="object-cover"
+                fill
+              />
+            </div>
+            <p className="text-xs text-muted-foreground">
+              {author}, {formatDate(publishedAt ?? "")}
+            </p>
+          </div>
+        </header>
+
+        <main className="prose mt-16 dark:prose-invert">
+          <MDXContent source={content} />
+        </main>
+
+        <footer className="mt-16">
+          <Subscribe />
+        </footer>
+      </div>
+    </section>
+  );
 }
+
 
 // import Link from "next/link";
 // import Image from "next/image";
